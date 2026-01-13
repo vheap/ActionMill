c41425db6ca2070b93a5e7b2edddaa230e07973a
Youssef <45735802+vheap@users.noreply.github.com>
2026-01-13 13:51:50 +0200

    Finalize the basis for logging. TODO: Addition of a some sor t of adapter for Micrisoft's logger.

---
+++ Logging/IActionMillLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActionMill.Logging
+{
+    public interface IActionMillLogger
+    {
+        // Using in to pass the struct by reference for performance, in case the LogEvent becomes larger.
+        void Log(in LogEvent e);
+    }
+}
---
+++ Logging/NullActionMillLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActionMill.Logging
+{
+    /// <summary>
+    /// Sealed class because it is not intended for inheritence. And it's optional, hence the null prefix to the class name.
+    public sealed class NullActionMillLogger : IActionMillLogger
+    {
+        public static readonly NullActionMillLogger Instance = new();
+        private NullActionMillLogger() { }
+        public void Log(in LogEvent e) { }
+    }
+}
